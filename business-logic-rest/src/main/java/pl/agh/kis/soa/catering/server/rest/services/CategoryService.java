6c1b0f7ab0af8ba7c3885b501f8db1e948349a76
Sig00rd <hamletolog@gmail.com>
2019-06-16 02:36:08 +0200

    Add rest api and dish categories service

---
+++ business-logic-rest/src/main/java/pl/agh/kis/soa/catering/server/rest/services/CategoryService.java
@@ -0,0 +1,47 @@
+package pl.agh.kis.soa.catering.server.rest.services;
+
+
+import io.swagger.annotations.Api;
+import pl.agh.kis.soa.catering.api.ICategoryRepository;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import java.util.List;
+
+@Path("category")
+@Api(value = "category", description = "Retrieving dish categories")
+public class CategoryService {
+    @EJB(lookup = "java:global/business-logic-implementation/CategoryRepository")
+    ICategoryRepository categoryRepository;
+
+    @GET
+    @Produces({ "text/xml", "application/xml", "application/json" })
+    public List<pl.agh.kis.soa.catering.model.Category> getAllCategories(@HeaderParam("Accept-Language") String language) {
+        if ("en".equals(language)) {
+            return getAllMenuCategoriesEN();
+        }
+        return getAllCategoriesPL();
+    }
+
+    @GET
+    @Produces({ "text/xml", "application/xml", "application/json" })
+    @Path("{menuCategoryId}")
+    public pl.agh.kis.soa.catering.model.Category getCategoryById(@PathParam("menuCategoryId")Long menuCategoryId) {
+        return categoryRepository.getCategory(menuCategoryId);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<pl.agh.kis.soa.catering.model.Category> getAllCategoriesPL() {
+        return (List<pl.agh.kis.soa.catering.model.Category>)(Object)categoryRepository.getAllCategories();
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<pl.agh.kis.soa.catering.model.Category> getAllMenuCategoriesEN() {
+        List<pl.agh.kis.soa.catering.model.Category> categories = (List<pl.agh.kis.soa.catering.model.Category>)(Object)categoryRepository.getAllCategories();
+
+        for(pl.agh.kis.soa.catering.model.Category category : categories)
+            category.setName(category.getName().toUpperCase());
+
+        return categories;
+    }
+}
