58a4900b643328aa61c80c1143ca784b81a40da1
Sig00rd <hamletolog@gmail.com>
2019-06-17 05:45:13 +0200

    Remove unused swagger stuff

--- business-logic-rest/src/main/java/pl/agh/kis/soa/catering/server/rest/Server.java
+++ business-logic-rest/src/main/java/pl/agh/kis/soa/catering/server/rest/Server.java
@@ -19,20 +19,7 @@ public class Server extends Application {
         final Set<Class<?>> classes = new HashSet<>();
 
         classes.add(CategoryService.class);
-//        classes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-//        classes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 
         return classes;
     }
-
-//    private void initSwagger() {
-//        BeanConfig beanConfig = new BeanConfig();
-//        beanConfig.setVersion("1.0.0");
-//        beanConfig.setSchemes(new String[]{"http"});
-//        beanConfig.setHost("localhost:8080");
-//        beanConfig.setBasePath("/business-logic-rest/api");
-//        beanConfig.setResourcePackage(CategoryService.class.getPackage().getName());
-//        beanConfig.setTitle("CateringApp Swagger");
-//        beanConfig.setScan(true);
-//    }
 }
