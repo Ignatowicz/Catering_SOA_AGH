09261e444865dc84b50bec6238686c48b2b62474
Sig00rd <hamletolog@gmail.com>
2019-06-17 03:30:40 +0200

    Add working client and api

--- business-logic-soap/src/main/java/pl/agh/kis/soa/server/soap/implementations/DishSoapServiceImpl.java
+++ business-logic-soap/src/main/java/pl/agh/kis/soa/server/soap/implementations/DishSoapServiceImpl.java
@@ -4,13 +4,14 @@ package pl.agh.kis.soa.server.soap.implementations;
 import pl.agh.kis.soa.catering.api.ICategoryRepository;
 import pl.agh.kis.soa.catering.api.IDishRepository;
 import pl.agh.kis.soa.catering.model.Dish;
-import pl.agh.kis.soa.server.soap.interfaces.ICateringSoapService;
+import pl.agh.kis.soa.server.soap.interfaces.DishSoapService;
 
 import javax.ejb.EJB;
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "pl.agh.kis.soa.server.soap.interfaces.ICateringSoapService")
-public class CateringSoapServiceImpl implements ICateringSoapService {
+@WebService(endpointInterface = "pl.agh.kis.soa.server.soap.interfaces.DishSoapService")
+public class DishSoapServiceImpl implements DishSoapService {
 
     @EJB(lookup = "java:global/business-logic-implementation/DishRepository")
     IDishRepository dishRepository;
@@ -18,6 +19,7 @@ public class CateringSoapServiceImpl implements ICateringSoapService {
     @EJB(lookup = "java:global/business-logic-implementation/CategoryRepository")
     ICategoryRepository categoryRepository;
 
+    @WebMethod
     public void addDishToCategory(String name, Float price, Long categoryId) {
         Dish dish = new Dish();
         dish.setAccepted(false);
